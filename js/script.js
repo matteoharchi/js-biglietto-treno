1fbe39b922adcc0a9856b596788b4e730d700018
Matteo Harchi <matteo.harchi@gmail.com>
2020-07-07 16:34:50 +0200

    Finito
    vedo se riesco a far funzionare arrotondamento a secondo decimale

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// PROMPT
+var kmviaggio = prompt("Quanti km dovrai percorrere?");
+var eta = prompt("Quanti anni hai?");
+// DATI FISSI
+var prezzokm = 0.21;
+var scontominori = 0.80;
+var scontosenior = 0.60;
+var prezzobiglietto = prezzokm * kmviaggio;
+// VARIABILI
+if (eta < 18) {
+    var prezzobiglietto = prezzobiglietto * scontominori;
+} else if (eta > 65) {
+    var prezzobiglietto = prezzobiglietto * scontosenior;
+}
+document.getElementById("prezzo-biglietto").innerHTML="Il prezzo del tuo biglietto è" + " " + prezzobiglietto;
